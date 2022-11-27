b537cc0df1114542c65d4e68e6396ad389a7a855
lohanson33@gmail.com <lohanson33@gmail.com>
2022-11-27 17:20:44 -0600

    Employee/Customer Views & Nav

---
+++ src/components/employees/EmployeeList.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { Employee } from "./Employee"
+import "./EmployeeList.css"
+
+export const EmployeeList = () => {
+
+    const [employees, setEmployees] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/users?isStaff=true`)
+                .then(response => response.json())
+                .then((employeeArray) => {
+                    setEmployees(employeeArray)
+                })
+        },
+        []
+    )
+
+    return (
+        <article className="employees">
+            {
+                employees.map(employee => <Employee key={`employee--${employee.id}`}
+
+                // id, fullName, email are all props
+                    id={employee.id} 
+                    fullName={employee.fullName} 
+                    email={employee.email} />) 
+            }
+        </article>
+    )
+}
