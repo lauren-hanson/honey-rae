fe6b458f7bf981cdcdc3a1311f861c9da67ad7d5
lohanson33@gmail.com <lohanson33@gmail.com>
2022-11-28 10:32:20 -0600

    EmployeeList/EmployeeDetails

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const Employee = ({ id, fullName, email }) => {
+
     return <section className="employee">
-        <div>Name: {fullName}</div>
+        <div>
+            <Link to={`/employees/${id}`}>Name: {fullName}</Link>
+        </div>
         <div>Email: {email}</div>
     </section>
-}
+} 
