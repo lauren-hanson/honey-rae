7c0a88f7224b4d674b8093fefe6a05f092cb445b
lohanson33@gmail.com <lohanson33@gmail.com>
2022-11-28 15:16:03 -0600

    TicketEdit

--- src/components/views/CustomerViews.js
+++ src/components/views/CustomerViews.js
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from "react-router-dom"
 import { TicketForm } from "../tickets/TicketForm"
 import { TicketList } from "../tickets/TicketList"
 import { Profile } from "../profile/Profile"
+import { TicketEdit } from "../tickets/TicketEdit"
 
 
 export const CustomerViews = () => {
@@ -19,6 +20,7 @@ export const CustomerViews = () => {
                 <Route path="tickets" element={<TicketList />} />
                 <Route path="ticket/create" element={<TicketForm />} />
                 <Route path="profile" element={<Profile />} />
+                <Route path="tickets/:ticketId/edit" element={ <TicketEdit /> } />
 
             </Route> 
         </Routes>
